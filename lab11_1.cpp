2a239a64b57a2a8276efe2fc006f06ab19ef7ed1
Your Name <you@example.com>
2023-01-03 17:19:07 +0700

    หิวข้าว

--- lab11_1.cpp
+++ lab11_1.cpp
@@ -2,25 +2,53 @@
 using namespace std;
 
 int main(){
-	int count[5] = {}; //Declare array count for counting A,B,C,D,F and initialize all element = 0
-	cout << "Please input grade of each student (A-F) or input 0 to exit.";
-	do{
-		cout << "Student [" << "]: ";
+    int i = 1 ;
+    char grade ;
+	int count[5] = {0,0,0,0,0}; //Declare array count for counting A,B,C,D,F and initialize all element = 0
+	cout << "Please input grade of each student (A-F) or input 0 to exit.\n";
+	do
+	{
+		cout << "Student [" << i << "]: " ;
 		cin >> grade; //The loop must be terminated when grade = '0'
-		if(true) // if grade is A
-			//Do something
-		}else if(true) // if grade is B
-			//Do something
-		//and so on ... for grade = C, D, F	
-		}else{ // grade is wrong input
-			//Do something
-		} 
-	}while(true);
+		if(grade == 'A')
+		{
+			count[0]++ ;
+		}
+		    else if(grade == 'B')
+		    {
+			    count[1]++ ;
+	    	}
+	    	else if(grade == 'C')
+	    	{
+		    	count[2]++ ;
+		    }
+		    else if(grade == 'D')
+		    {
+		    	count[3]++ ;
+		    }
+		    else if(grade == 'F')
+		    {
+			    count[4]++ ;
+		    }
+		    else if(grade == '0')
+		    {
+	            i= i-2 ;
+		    }
+		    else
+		    {
+	            cout << "Wrong input. Please input again.\n" ;
+                i-- ;
+		    }
+	    i++	 ;
+	}while(grade != '0') ;
 	
 	
-	cout << "In total ? students.";
+	cout << "In total " << i << " students.\n";
 	cout << "A = " << count[0] <<", ";
-	cout << "B = " << count[1] <<", ";	
+	cout << "B = " << count[1] <<", ";
+	cout << "C = " << count[2] <<", ";
+	cout << "D = " << count[3] <<", ";
+	cout << "F = " << count[4];
 	//	and so on ... for grade = C, D, F	
 	
 	return 0;
--- lab11_3.cpp
+++ lab11_3.cpp
@@ -1,3 +1,28 @@
-//cout << "Number of data = ";
-//cout << "Mean = ";
-//cout << "Standard deviation = ";
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <cstdlib>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+    ifstream source;
+	source.open("score.txt");
+	double M , S , A , A2 ; 
+	int N = 0 ;
+	string textline ;
+	while (getline(source,textline))
+	{
+        A = A+stof(textline) ; 
+        A2 = A2+pow(stof(textline),2) ;
+	    N++ ;
+	}
+	M = A/N ;
+    S = sqrt((A2/N)-pow(M,2)) ;
+    cout << "Number of data = " << N <<"\n";
+    cout << setprecision(3);
+    cout << "Mean = " << M << "\n" ;
+    cout << "Standard deviation = " << S << "\n";
+}
